a5018a09589a97dc9040f49c479f6b851e4c5ff6
benjamin <benjamin.dartigues@u-bordeaux.fr>
2019-06-11 09:58:25 +0200

    chnage arguments order in compute degree of clustering

--- analysis/analysis_correlation_profile/main.py
+++ analysis/analysis_correlation_profile/main.py
@@ -48,7 +48,7 @@ def main():
                 cyt_total=adsc.compute_cytoplasmic_total(image_list, file_handler, path.path_data)
                 prof_m[cpt_g, 2, cpt_tp]=np.median(cyt_total)
 
-                dof = adsc.compute_degree_of_clustering(hstar_file_handler, mtoc_file_handler, image_list)
+                dof = adsc.compute_degree_of_clustering(image_list,hstar_file_handler, mtoc_file_handler)
                 prof_m[cpt_g, 3, cpt_tp]=np.median(dof)
 
                 cpt_tp+=1
@@ -80,7 +80,7 @@ def main():
                 cyt_total = adsc.compute_cytoplasmic_total(image_list, file_handler, path.path_data)
                 prof_p[cpt_g, 2, cpt_tp] = np.median(cyt_total)
 
-                dof = adsc.compute_degree_of_clustering(hstar_file_handler, mtoc_file_handler, image_list)
+                dof = adsc.compute_degree_of_clustering(image_list, mtoc_file_handler, hstar_file_handler)
                 prof_p[cpt_g, 3, cpt_tp] = np.median(dof)
 
                 cpt_tp += 1
