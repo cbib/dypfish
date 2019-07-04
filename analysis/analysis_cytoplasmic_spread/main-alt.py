8ce687f65814fe719c89bd24423d7a41fc3098fc
Thierry Arsicaud <t.arsicaud@catie.fr>
2019-07-04 16:09:38 +0200

    handling raw images dir path and external logger

--- analysis/analysis_cytoplasmic_spread/main-alt.py
+++ analysis/analysis_cytoplasmic_spread/main-alt.py
@@ -17,12 +17,12 @@ PNG_IMAGES_MIME_TYPE = "image/png"
 CYTOPLASMIC_SPREAD_GRAPHS_SUFFIX = "cytoplasmic_spread"
 
 def cytoplasmic_spread(
-    raw_images_dir_path_name,
     basic_h5_file_handler,
     genes,
     molecule_type,
     save_into_dir_path_name,
-    logger=None
+    raw_images_dir_path_name=None,
+    ext_logger=None
     ):  # TODO argument genes vs proteins ?
     """
     * Generates a cytoplasmic spread graph and saves it in a file
@@ -35,12 +35,12 @@ def cytoplasmic_spread(
     - molecule_type : a string (molecule type ID)
       Ex. : "mrna" or "protein"
     - save_into_dir_path_name : a string (path of the directory where the graph will be saved)
-    - logger : external logger
+    - ext_logger : external logger
     """
     assert os.path.isdir(save_into_dir_path_name)
 
-    if logger:
-        logger.debug("Generating cytoplasmic spread graph (%s)..." % molecule_type)
+    if ext_logger:
+        ext_logger.debug("Generating cytoplasmic spread graph (%s)..." % molecule_type)
 
     graph_file_name = "%s_%s.png" % (molecule_type, CYTOPLASMIC_SPREAD_GRAPHS_SUFFIX)
     graph_file_path_name = os.path.join(save_into_dir_path_name, graph_file_name)
@@ -69,8 +69,8 @@ def cytoplasmic_spread(
     plot.bar_profile(cyt_spreads, genes, graph_file_path_name)
     assert(os.path.isfile(graph_file_path_name))
 
-    if logger:
-        logger.debug("Graph generated on path : %s" % graph_file_path_name)
+    if ext_logger:
+        ext_logger.debug("Graph generated on path : %s" % graph_file_path_name)
 
     return {graph_file_path_name: graph_metadata}
 
@@ -79,12 +79,12 @@ def cytoplasmic_spread(
 CYTOPLASMIC_SPREAD_DYNAMIC_PROFILES_GRAPHS_PREFIX = "cyt_spread"
 
 def cytoplasmic_spread_dynamic_profiles(
-    raw_images_dir_path_name,
     basic_h5_file_handler,
     genes,
     proteins,
     save_into_dir_path_name,
-    logger=None
+    raw_images_dir_path_name=None,
+    ext_logger=None
     ):
     """
     * Generates cytoplasmic spread dynamic profiles and saves them in files
@@ -101,13 +101,13 @@ def cytoplasmic_spread_dynamic_profiles(
     assert os.path.isdir(save_into_dir_path_name)
 
     # This part produces plot interpolation of cytoplasmic spread by timepoint
-    if logger:
-        logger.debug("Generating cytoplasmic spread dynamic profiles...")
+    if ext_logger:
+        ext_logger.debug("Generating cytoplasmic spread dynamic profiles...")
 
     graphs_details = []
 
-    if logger:
-        logger.debug("Extracting data...")
+    if ext_logger:
+        ext_logger.debug("Extracting data...")
 
     data_generator = plot.data_extractor(
         genes,
@@ -123,12 +123,12 @@ def cytoplasmic_spread_dynamic_profiles(
             gene = genes[i]
             colour = plot_colors[i]
 
-            if logger:
-                logger.debug("Processing gene : %s..." % gene)
+            if ext_logger:
+                ext_logger.debug("Processing gene : %s..." % gene)
 
             try:
-                if logger:
-                    logger.debug("Generating graph %s..." % graph_file_name)
+                if ext_logger:
+                    ext_logger.debug("Generating graph %s..." % graph_file_name)
 
                 graph_file_name = "%s_%s.png" % (CYTOPLASMIC_SPREAD_DYNAMIC_PROFILES_GRAPHS_PREFIX, gene)
                 graph_file_path_name = os.path.join(save_into_dir_path_name, graph_file_name)
@@ -149,20 +149,20 @@ def cytoplasmic_spread_dynamic_profiles(
 
                 assert(os.path.isfile(graph_file_path_name))
 
-                if logger:
-                    logger.debug("Graph generated on path : %s" % graph_file_path_name)
+                if ext_logger:
+                    ext_logger.debug("Graph generated on path : %s" % graph_file_path_name)
 
                 graphs_details.append({graph_file_path_name: graph_metadata})
 
             except Exception as e:
-                if logger:
-                    logger.exception(
+                if ext_logger:
+                    ext_logger.exception(
                         "Could not generate cytoplasmic spread dynamic profile for gene : %s" % gene
                         )
 
     except Exception as e:
-        if logger:
-            logger.exception(
+        if ext_logger:
+            ext_logger.exception(
                 "Got exception ! (maybe raised by data_generator ?)"
                 )
 
@@ -170,13 +170,12 @@ def cytoplasmic_spread_dynamic_profiles(
 
 
 def main(
-    raw_images_dir_path_name,
     basic_h5_file_path_name,
-    save_into_dir_path_name
+    save_into_dir_path_name,
+    raw_images_dir_path_name=None,
+    ext_logger=None
     ):
     # Required descriptors: spots, IF, zero level, cell mask, nucleus_centroid and height_map
-    enable_logger()
-
     resulting_graphs_details_as_list = []
 
     # Compute bar plot cytoplasmic spread
@@ -186,29 +185,32 @@ def main(
     with h5py.File(basic_h5_file_path_name, "r") as basic_h5_file_handler:
 
         graph_details = cytoplasmic_spread(
-            raw_images_dir_path_name=raw_images_dir_path_name,
             basic_h5_file_handler=basic_h5_file_handler,
             genes=genes,
             molecule_type='mrna',
-            save_into_dir_path_name=save_into_dir_path_name
+            save_into_dir_path_name=save_into_dir_path_name,
+            raw_images_dir_path_name=raw_images_dir_path_name,
+            ext_logger=ext_logger
             )
         resulting_graphs_details_as_list.append(graph_details)
 
         graph_details = cytoplasmic_spread(
-            raw_images_dir_path_name=raw_images_dir_path_name,
             basic_h5_file_handler=basic_h5_file_handler,
             genes=proteins,
             molecule_type='protein',
-            save_into_dir_path_name=save_into_dir_path_name
+            save_into_dir_path_name=save_into_dir_path_name,
+            raw_images_dir_path_name=raw_images_dir_path_name,
+            ext_logger=ext_logger
             )
         resulting_graphs_details_as_list.append(graph_details)
 
         graphs_details = cytoplasmic_spread_dynamic_profiles(
-            raw_images_dir_path_name=raw_images_dir_path_name,
             basic_h5_file_handler=basic_h5_file_handler,
             genes=genes,
             proteins=proteins,
-            save_into_dir_path_name=save_into_dir_path_name
+            save_into_dir_path_name=save_into_dir_path_name,
+            raw_images_dir_path_name=raw_images_dir_path_name,
+            ext_logger=ext_logger
             )
         resulting_graphs_details_as_list += graphs_details
 
@@ -221,14 +223,14 @@ def main(
 
 if __name__ == "__main__":
 
-    raw_images_dir_path_name = path.path_data
+    enable_logger()
+
     basic_h5_file_path_name = path.basic_file_path
     save_into_dir_path_name = os.path.join(path.analysis_dir, "analysis_cytoplasmic_spread/figures/")
     if not os.path.isdir(save_into_dir_path_name):
         os.mkdir(save_into_dir_path_name)
 
     resulting_graphs_details = main(
-        raw_images_dir_path_name,
         basic_h5_file_path_name,
         save_into_dir_path_name
         )
