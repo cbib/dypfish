cf746329a04d1688542e4de3095c46dfd2506b72
Thierry Arsicaud <t.arsicaud@catie.fr>
2019-07-04 17:55:32 +0200

    refactored cytoplasmic total count script

--- analysis/analysis_cytoplasmic_total_count/main-alt.py
+++ analysis/analysis_cytoplasmic_total_count/main-alt.py
@@ -6,7 +6,7 @@ import os
 from collections import OrderedDict
 import h5py
 import src.plot as plot
-from src import acquisition_descriptors as adsc
+import src.acquisition_descriptors as adsc
 import src.path as path
 import src.helpers as helps
 from src.utils import enable_logger, plot_colors
@@ -14,59 +14,49 @@ from src.utils import enable_logger, plot_colors
 
 PNG_IMAGES_MIME_TYPE = "image/png"
 
-CYTOPLASMIC_SPREAD_GRAPHS_SUFFIX = "cytoplasmic_spread"
+CYTOPLASMIC_TOTAL_COUNT_GRAPHS_SUFFIX = "total_cytoplasmic_transcript"
 
-def cytoplasmic_spread(
+def cytoplasmic_total_count(
     basic_h5_file_handler,
-    genes,
     molecule_type,
+    genes,
     save_into_dir_path_name,
     raw_images_dir_path_name=None,
     ext_logger=None
-    ):  # TODO argument genes vs proteins ?
-    """
-    * Generates a cytoplasmic spread graph and saves it in a file
-    * Returns a tuple embedding 2 values : temporary file path name,
-      associated metadata (as a dict)
-    * Parameters :
-    - basic_h5_file_handler : handler linked to a 'basic.h5' file
-    - genes : list of strings (genes IDs)
-      Ex. : ["beta_actin", "arhgdia", "gapdh", "pard3", "pkp4", "rab13"]
-    - molecule_type : a string (molecule type ID)
-      Ex. : "mrna" or "protein"
-    - save_into_dir_path_name : a string (path of the directory where the graph will be saved)
-    - ext_logger : external logger
-    """
+    ):
     assert os.path.isdir(save_into_dir_path_name)
 
     if ext_logger:
-        ext_logger.debug("Generating cytoplasmic spread graph (%s)..." % molecule_type)
+        ext_logger.debug("Generating cytoplasmic total count graphs (%s)..." % molecule_type)
 
-    graph_file_name = "%s_%s.png" % (molecule_type, CYTOPLASMIC_SPREAD_GRAPHS_SUFFIX)
+    cytoplasmic_total = []
+    for gene in genes:
+        if ext_logger:
+            ext_logger.debug("Processing gene %s..." % gene)
+
+        image_list = helps.preprocess_image_list2(
+            basic_h5_file_handler,
+            '/' + molecule_type,
+            gene
+            )
+
+        cytoplasmic_total.append(
+            adsc.compute_cytoplasmic_total(
+                image_list,
+                basic_h5_file_handler,
+                path_data=raw_images_dir_path_name
+                )
+            )
+
+    graph_file_name = "%s_%s.png" % (molecule_type, CYTOPLASMIC_TOTAL_COUNT_GRAPHS_SUFFIX)
     graph_file_path_name = os.path.join(save_into_dir_path_name, graph_file_name)
     graph_metadata = {
         "file_name": graph_file_name,
         "mime_type": PNG_IMAGES_MIME_TYPE
         }
 
-    # for each gene in genes list, compute cytoplasmic spread
-    cyt_spreads = []
+    plot.bar_profile(cytoplasmic_total, genes, graph_file_path_name)
 
-    for gene in genes:
-        image_list = helps.preprocess_image_list2(
-            basic_h5_file_handler,
-            '/' + molecule_type,
-            gene
-            )
-        cyt_spreads.append(
-            adsc.compute_cytoplasmic_spread(
-                image_list,
-                basic_h5_file_handler,
-                raw_images_dir_path_name
-                )
-            )
-
-    plot.bar_profile(cyt_spreads, genes, graph_file_path_name)
     assert(os.path.isfile(graph_file_path_name))
 
     if ext_logger:
@@ -75,10 +65,9 @@ def cytoplasmic_spread(
     return {graph_file_path_name: graph_metadata}
 
 
+CYTOPLASMIC_TOTAL_COUNT_DYNAMIC_PROFILES_GRAPHS_PREFIX = "cyt_total"
 
-CYTOPLASMIC_SPREAD_DYNAMIC_PROFILES_GRAPHS_PREFIX = "cyt_spread"
-
-def cytoplasmic_spread_dynamic_profiles(
+def cytoplasmic_total_dynamic_profiles(
     basic_h5_file_handler,
     genes,
     proteins,
@@ -86,39 +75,24 @@ def cytoplasmic_spread_dynamic_profiles(
     raw_images_dir_path_name=None,
     ext_logger=None
     ):
-    """
-    * Generates cytoplasmic spread dynamic profiles and saves them in files
-    * Returns a list of tuples embedding 2 values :
-      temporary file path name, associated metadata (as a dict)
-    * Parameters :
-    - basic_h5_file_handler : handler linked to a 'basic.h5' file
-    - genes : list of strings (genes IDs)
-      Ex. : ["beta_actin", "arhgdia", "gapdh", "pard3", "pkp4", "rab13"]
-    - proteins :  list of strings (proteins IDs)
-      Ex. : ["beta_actin", "arhgdia", "gapdh", "pard3"]
-    - save_into_dir_path_name : a string (path of the directory where the graphs will be saved)
-    """
     assert os.path.isdir(save_into_dir_path_name)
 
-    # This part produces plot interpolation of cytoplasmic spread by timepoint
     if ext_logger:
-        ext_logger.debug("Generating cytoplasmic spread dynamic profiles...")
+        ext_logger.debug("Generating cytoplasmic total count dynamic profiles.")
 
     graphs_details = []
-
-    if ext_logger:
-        ext_logger.debug("Extracting data...")
 
     data_generator = plot.data_extractor(
         genes,
         proteins,
         basic_h5_file_handler,
-        adsc.compute_cytoplasmic_spread,
+        adsc.compute_cytoplasmic_total,
         basic_h5_file_handler,
         raw_images_dir_path_name
         )
 
     try:
+
         for mrna_data, protein_data, i in data_generator:
             gene = genes[i]
             colour = plot_colors[i]
@@ -127,7 +101,7 @@ def cytoplasmic_spread_dynamic_profiles(
                 ext_logger.debug("Processing gene : %s..." % gene)
 
             try:
-                graph_file_name = "%s_%s.png" % (CYTOPLASMIC_SPREAD_DYNAMIC_PROFILES_GRAPHS_PREFIX, gene)
+                graph_file_name = "%s_%s.png" % (CYTOPLASMIC_TOTAL_COUNT_DYNAMIC_PROFILES_GRAPHS_PREFIX, gene)
                 graph_file_path_name = os.path.join(save_into_dir_path_name, graph_file_name)
                 graph_metadata = {
                     "file_name": graph_file_name,
@@ -143,7 +117,7 @@ def cytoplasmic_spread_dynamic_profiles(
                     gene,
                     colour,
                     'Time(hrs)',
-                    'Cytoplasmic spread',
+                    'Cytoplasmic total',
                     graph_file_path_name
                     )
 
@@ -159,16 +133,15 @@ def cytoplasmic_spread_dynamic_profiles(
                     ext_logger.exception(
                         "Could not generate cytoplasmic spread dynamic profile for gene : %s" % gene
                         )
-                raise
 
     except Exception as e:
         if ext_logger:
             ext_logger.exception(
                 "Got exception ! (maybe raised by data_generator ?)"
                 )
-        raise
 
     return graphs_details
+
 
 
 def main(
@@ -177,36 +150,37 @@ def main(
     raw_images_dir_path_name=None,
     ext_logger=None
     ):
-    # Required descriptors: spots, IF, zero level, cell mask, nucleus_centroid and height_map
+
     resulting_graphs_details_as_list = []
 
-    # Compute bar plot cytoplasmic spread
+    # Required descriptors: spots, IF, cell mask an height_map
+    # Compute bar plot cytoplasmic total transcripts
     genes = ["beta_actin", "arhgdia", "gapdh", "pard3", "pkp4", "rab13"]
     proteins = ["beta_actin", "arhgdia", "gapdh", "pard3"]
 
     with h5py.File(basic_h5_file_path_name, "r") as basic_h5_file_handler:
 
-        graph_details = cytoplasmic_spread(
+        graph_details = cytoplasmic_total_count(
             basic_h5_file_handler=basic_h5_file_handler,
-            genes=genes,
             molecule_type='mrna',
+            genes=genes,
             save_into_dir_path_name=save_into_dir_path_name,
             raw_images_dir_path_name=raw_images_dir_path_name,
             ext_logger=ext_logger
             )
         resulting_graphs_details_as_list.append(graph_details)
 
-        graph_details = cytoplasmic_spread(
+        graph_details = cytoplasmic_total_count(
             basic_h5_file_handler=basic_h5_file_handler,
-            genes=proteins,
             molecule_type='protein',
+            genes=proteins,
             save_into_dir_path_name=save_into_dir_path_name,
             raw_images_dir_path_name=raw_images_dir_path_name,
             ext_logger=ext_logger
             )
         resulting_graphs_details_as_list.append(graph_details)
 
-        graphs_details = cytoplasmic_spread_dynamic_profiles(
+        graphs_details = cytoplasmic_total_dynamic_profiles(
             basic_h5_file_handler=basic_h5_file_handler,
             genes=genes,
             proteins=proteins,
@@ -228,7 +202,7 @@ if __name__ == "__main__":
     enable_logger()
 
     basic_h5_file_path_name = path.basic_file_path
-    save_into_dir_path_name = os.path.join(path.analysis_dir, "analysis_cytoplasmic_spread/figures/")
+    save_into_dir_path_name = os.path.join(path.analysis_dir, "analysis_cytoplasmic_total_count/figures/")
     if not os.path.isdir(save_into_dir_path_name):
         os.mkdir(save_into_dir_path_name)
 
