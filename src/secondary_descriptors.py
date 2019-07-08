7af5cad5eb7b0fab2cde5541fad58d3f67baa3b7
Thierry Arsicaud <t.arsicaud@catie.fr>
2019-07-08 17:13:11 +0200

    refactoring secondary descriptors gen script

--- src/secondary_descriptors.py
+++ src/secondary_descriptors.py
@@ -22,13 +22,16 @@ import matplotlib.pyplot as plt
 import math
 from image_descriptors import *
 
-def compute_cell_mask_3d(file_handler,image):
-   height_map = get_height_map(file_handler, image)
+def compute_cell_mask_3d(
+    basic_h5_file_handler,
+    image
+    ):
+   height_map = get_height_map(basic_h5_file_handler, image)
    if height_map.size == 0:
        return None
 
-   zero_level = get_zero_level(file_handler, image)
-   cell_mask = get_cell_mask(file_handler, image)
+   zero_level = get_zero_level(basic_h5_file_handler, image)
+   cell_mask = get_cell_mask(basic_h5_file_handler, image)
    height_map = height_map.astype(float)
    height_map[cell_mask == 0] = np.nan
    reversed_height_map = zero_level-height_map+1
@@ -43,34 +46,42 @@ def compute_cell_mask_3d(file_handler,image):
        slice_mask[slice_mask > slice] = np.nan
        slice_mask[slice_mask <= float(slice)] = 1
        slice_mask=np.nan_to_num(slice_mask)
-       cell_masks[:,:,slice] = slice_mask
-   return cell_masks
+       cell_masks[:,:,slice]
 
 
-def set_h_star_mrna(file_handler, output_file_handler, image):
+def set_h_star_mrna(
+    basic_h5_file_handler,
+    secondary_h5_file_handler,
+    image
+    ):
 
-    H_star = get_h_star(file_handler, image)
+    H_star = get_h_star(basic_h5_file_handler, image)
     assert (not H_star.size), 'H_star already defined for %r' % image
 
     # get descriptors
-    spots = get_spots(file_handler, image)
+    spots = get_spots(basic_h5_file_handler, image)
 
-    cell_mask_3d = compute_cell_mask_3d(file_handler, image)
+    cell_mask_3d = compute_cell_mask_3d(basic_h5_file_handler, image)
     if cell_mask_3d is None:
         print("[set_h_star_mrna] : Skipping hstar")
         return False
     #h_star=helps.clustering_index_point_process_2d(spots, cell_mask_3d)
     h_star=helps.clustering_index_point_process(spots, cell_mask_3d)
     descriptor = image + '/h_star'
-    #output_file_handler[descriptor][:] = h_star
-    output_file_handler.create_dataset(descriptor, data=h_star, dtype=np.float32)
+    #secondary_h5_file_handler[descriptor][:] = h_star
+    secondary_h5_file_handler.create_dataset(descriptor, data=h_star, dtype=np.float32)
 
-    
-def set_h_star_protein(file_handler, output_file_handler, image):
+
+def set_h_star_protein(
+    basic_h5_file_handler,
+    secondary_h5_file_handler,
+    raw_images_dir_path,
+    image
+    ):
 
     def prune_intensities(image, zero_level):
         molecule, gene, timepoint, number = image.split("/")
-        IF_image_path = path.raw_data_dir + gene + '/' + molecule + '_' + timepoint + "/image_" + number + '/IF.tif'
+        IF_image_path = raw_images_dir_path + gene + '/' + molecule + '_' + timepoint + "/image_" + number + '/IF.tif'
         IF = io.imread(IF_image_path, plugin='tifffile')
 
         vol_block = np.zeros((512, 512, zero_level))
@@ -79,24 +90,28 @@ def set_h_star_protein(file_handler, output_file_handler, image):
 
         return vol_block  # .reshape((512,512,zero_level + constants.VOLUME_OFFSET))
 
-    cell_mask_3d=compute_cell_mask_3d(file_handler,image)
+    cell_mask_3d=compute_cell_mask_3d(basic_h5_file_handler,image)
     if cell_mask_3d is None:
         print("[set_h_star_mrna] : Skipping hstar")
         return False
-    zero_level=get_zero_level(file_handler,image)
+    zero_level=get_zero_level(basic_h5_file_handler,image)
     IF = prune_intensities(image, zero_level)
     IF = IF * cell_mask_3d
     h_star = helps.clustering_index_random_measure(IF, cell_mask_3d)
     descriptor = image + '/h_star'
-    # output_file_handler[descriptor][:] = h_star
-    output_file_handler.create_dataset(descriptor, data=h_star, dtype=np.float32)
+    # secondary_h5_file_handler[descriptor][:] = h_star
+    secondary_h5_file_handler.create_dataset(descriptor, data=h_star, dtype=np.float32)
 
 
-def set_zero_level(file_handler, image, raw_data_dir):
+def set_zero_level(
+    basic_h5_file_handler,
+    image,
+    raw_images_dir_path
+    ):
     molecule, gene, timepoint, number = image.split("/")
-    tubulin_image_path = raw_data_dir+gene+'/'+molecule+'_'+timepoint+"/image_"+number+"/tubulin.tif"
+    tubulin_image_path = raw_images_dir_path+gene+'/'+molecule+'_'+timepoint+"/image_"+number+"/tubulin.tif"
     if not os.path.exists(tubulin_image_path):
-        tubulin_image_path = raw_data_dir + gene + '/' + molecule + '_' + timepoint +"/"+ number + "/tubulin.tif"
+        tubulin_image_path = raw_images_dir_path + gene + '/' + molecule + '_' + timepoint +"/"+ number + "/tubulin.tif"
 
     image_stacked = io.imread(tubulin_image_path, plugin='tifffile')
     #print(image_stacked.shape)
@@ -109,7 +124,7 @@ def set_zero_level(file_handler, image, raw_data_dir):
     '''
     if len(image_stacked.shape) == 2:  # TODO find a way to make things generic here by adapting to both 2d and 3d cases
         print ("This image is not a 3D one, removing it from basic.h5 to prevent further compatibility issues")
-        del file_handler[image]
+        del basic_h5_file_handler[image]
         return
     z_size, x_size, y_size = image_stacked.shape
 
@@ -118,24 +133,34 @@ def set_zero_level(file_handler, image, raw_data_dir):
     for n in range(z_size):
         slice = image_stacked[n, :, :] if z_size > 1 else  image_stacked[ :, :]
         sums[n] = slice.sum()
-    file_handler[image].attrs['zero_level'] = sums.argmax()
+    basic_h5_file_handler[image].attrs['zero_level'] = sums.argmax()
 
 
-def set_3d_spots(file_handler, image):
-    if file_handler[image]['spots'].shape[1] == 3:
+def set_3d_spots(
+    basic_h5_file_handler,
+    image
+    ):
+    if basic_h5_file_handler[image]['spots'].shape[1] == 3:
         return
     spots_3d = []
-    for spot in file_handler[image]['spots']:
-        spots_3d.append((spot[0],spot[1], file_handler[image]['height_map'][spot[0],spot[1]]))
+    for spot in basic_h5_file_handler[image]['spots']:
+        spots_3d.append((spot[0],spot[1], basic_h5_file_handler[image]['height_map'][spot[0],spot[1]]))
 
-    del file_handler[image]['spots']
-    file_handler.create_dataset(image+'/spots', data=spots_3d, dtype=np.float32)
+    del basic_h5_file_handler[image]['spots']
+    basic_h5_file_handler.create_dataset(image+'/spots', data=spots_3d, dtype=np.float32)
+
 
 
 '''Cell mask distance map computation'''
 # Given the nucleus and cytoplasm mask and x, y slopes of a line, compute the segment
 # of the line that falls withing the nucleus and the segment that falls within the cytoplasm
-def compute_line_segments(nucleus_mask, cytoplasm_mask, nucleus_centroid, x_slope, y_slope):
+def compute_line_segments(
+    nucleus_mask,
+    cytoplasm_mask,
+    nucleus_centroid,
+    x_slope,
+    y_slope
+    ):
     logger.info('Compute line segments')
     cytoplasm_segment = np.zeros(constants.MAX_CELL_RADIUS)
     nucleus_segment = np.zeros(constants.MAX_CELL_RADIUS)
@@ -159,7 +184,10 @@ def compute_line_segments(nucleus_mask, cytoplasm_mask, nucleus_centroid, x_slop
 
 
 # Given line segments, return points that fall on the nucleus and on the cytoplasm edges
-def compute_edge_points(nucleus_segment, cytoplasm_segment):
+def compute_edge_points(
+    nucleus_segment,
+    cytoplasm_segment
+    ):
     nucleus_points = np.where(nucleus_segment == 1)
     if len(nucleus_points[0]) > 0:
         nucleus_edge_point = nucleus_points[0][-1]
@@ -174,7 +202,11 @@ def compute_edge_points(nucleus_segment, cytoplasm_segment):
     return nucleus_edge_point, cytoplasm_edge_point
 
 
-def compute_cell_mask_distance_map(nucleus_mask, cytoplasm_mask, contour_points):
+def compute_cell_mask_distance_map(
+    nucleus_mask,
+    cytoplasm_mask,
+    contour_points
+    ):
     cell_mask_distance_map = np.zeros((512, 512), dtype=np.int)
     for index in range(constants.NUM_CONTOURS):
         if index == 0:
@@ -192,16 +224,20 @@ def compute_cell_mask_distance_map(nucleus_mask, cytoplasm_mask, contour_points)
 
 # Create a map with concentric isolines subdividing the cytoplasm into slices NUM_CONTOURS
 # Each isoline is built by constructing a polygone from 360 points (one point per degree)
-def set_cell_mask_distance_map(file_handler, output_file_handler, image):
+def set_cell_mask_distance_map(
+    basic_h5_file_handler,
+    secondary_h5_file_handler,
+    image
+    ):
 
     #descriptor = image + '/cell_mask_distance_map'
-    #assert (descriptor not in output_file_handler.keys(), 'cell_mask_distance_map already defined for %r' % image)
+    #assert (descriptor not in secondary_h5_file_handler.keys(), 'cell_mask_distance_map already defined for %r' % image)
 
-    cell_mask_distance_map = get_cell_mask_distance_map(file_handler, image)
+    cell_mask_distance_map = get_cell_mask_distance_map(basic_h5_file_handler, image)
     assert (not cell_mask_distance_map.size), 'cell_mask_distance_map already defined for %r' % image
-    cell_mask = get_cell_mask(file_handler, image).astype(int)
-    nucleus_mask = get_nucleus_mask(file_handler, image).astype(int)
-    nucleus_centroid = get_nucleus_centroid(file_handler, image).transpose()
+    cell_mask = get_cell_mask(basic_h5_file_handler, image).astype(int)
+    nucleus_mask = get_nucleus_mask(basic_h5_file_handler, image).astype(int)
+    nucleus_centroid = get_nucleus_centroid(basic_h5_file_handler, image).transpose()
     contour_points = np.zeros((360, 100, 2))
     cytoplasm_mask = (cell_mask == 1) & (nucleus_mask == 0)
 
@@ -233,37 +269,54 @@ def set_cell_mask_distance_map(file_handler, output_file_handler, image):
     # plt.imshow(cell_mask_distance_map, cmap='hot')
     # plt.show()
     descriptor = image + '/cell_mask_distance_map'
-    output_file_handler.create_dataset(descriptor, data=cell_mask_distance_map, dtype=np.int)
+    secondary_h5_file_handler.create_dataset(descriptor, data=cell_mask_distance_map, dtype=np.int)
 
 
 '''Cell and nucleus area computation'''
-def set_cell_area(file_handler, output_file_handler, image):
+def set_cell_area(
+    basic_h5_file_handler,
+    secondary_h5_file_handler,
+    image
+    ):
     """compute cell surface by pixel using cell mask"""
-    cell_area = get_cell_area(output_file_handler, image)
+    cell_area = get_cell_area(secondary_h5_file_handler, image)
     assert (cell_area == -1), 'cell_area already defined for %r' % image
 
     #descriptor = image + '/cell_area'
-    #assert (descriptor not in output_file_handler.attrs.keys()), 'cell_area already defined for %r' % image
+    #assert (descriptor not in secondary_h5_file_handler.attrs.keys()), 'cell_area already defined for %r' % image
 
-    cell_mask = get_cell_mask(file_handler, image)
+    cell_mask = get_cell_mask(basic_h5_file_handler, image)
     area = cell_mask.sum() * math.pow((1 / constants.SIZE_COEFFICIENT), 2)  # * by pixel dimensions
-    output_file_handler[image].attrs['cell_area'] = area
+    secondary_h5_file_handler[image].attrs['cell_area'] = area
 
-def set_nucleus_area(file_handler, output_file_handler, image):
+def set_nucleus_area(
+    basic_h5_file_handler,
+    secondary_h5_file_handler,
+    image
+    ):
     """compute nucleus surface in pixel using nucleus mask"""
-    nucleus_area = get_nucleus_area(output_file_handler, image)
+    nucleus_area = get_nucleus_area(secondary_h5_file_handler, image)
     assert (nucleus_area == -1), 'nucleus_area already defined for %r' % image
-    nucleus_mask = get_nucleus_mask(file_handler, image)
+    nucleus_mask = get_nucleus_mask(basic_h5_file_handler, image)
     area = nucleus_mask.sum() * math.pow((1 / constants.SIZE_COEFFICIENT), 2)  # * by pixel dimensions
-    output_file_handler[image].attrs['nucleus_area'] = area
+    secondary_h5_file_handler[image].attrs['nucleus_area'] = area
 
 
 from threading import Thread
 
 class Preprocess(Thread):
 
-    def __init__(self, img_list):
+    def __init__(
+        self,
+        basic_h5_file_handler,
+        secondary_h5_file_handler,
+        raw_images_dir_path,
+        img_list
+        ):
         Thread.__init__(self)
+        self.basic_h5_file_handler = basic_h5_file_handler
+        self.secondary_h5_file_handler = secondary_h5_file_handler
+        self.raw_images_dir_path = raw_images_dir_path
         self.img_list = img_list
 
     def run(self):
@@ -271,48 +324,118 @@ class Preprocess(Thread):
             print("Computing descriptors for " + image)
 
             '''UPDATE BASIC.H5'''
-            set_zero_level(input_file_handler, image, path.raw_data_dir)
+            set_zero_level(
+                self.basic_h5_file_handler,
+                image,
+                self.raw_images_dir_path
+                )
 
             '''PREPROCESS'''
-            set_cell_mask_distance_map(input_file_handler, output_file_handler, image)
+            set_cell_mask_distance_map(
+                self.basic_h5_file_handler,
+                self.secondary_h5_file_handler,
+                image
+                )
             if 'mrna' in image:
-                set_3d_spots(input_file_handler, image)
-                set_spots_peripheral_distance(input_file_handler, output_file_handler, image)
-                set_spots_peripheral_distance_2D(input_file_handler, output_file_handler, image)
-            set_cell_area(input_file_handler, output_file_handler, image)
-            set_nucleus_area(input_file_handler, output_file_handler, image)
+                set_3d_spots(
+                    self.basic_h5_file_handler,
+                    image
+                    )
+                set_spots_peripheral_distance(
+                    self.basic_h5_file_handler,
+                    self.secondary_h5_file_handler,
+                    image
+                    )
+                set_spots_peripheral_distance_2D(
+                    self.basic_h5_file_handler,
+                    self.secondary_h5_file_handler,
+                    image
+                    )
+            set_cell_area(
+                self.basic_h5_file_handler,
+                self.secondary_h5_file_handler,
+                image
+                )
+            set_nucleus_area(
+                self.basic_h5_file_handler,
+                self.secondary_h5_file_handler,
+                image
+                )
             '''PREPROCESS'''
 
             if 'mrna' in image:
-                set_h_star_mrna(input_file_handler, output_file_handler, image)
+                set_h_star_mrna(
+                    self.basic_h5_file_handler,
+                    self.secondary_h5_file_handler,
+                    image
+                    )
             elif 'protein' in image:
-                set_h_star_protein(input_file_handler, output_file_handler, image)
+                set_h_star_protein(
+                    self.basic_h5_file_handler,
+                    self.secondary_h5_file_handler,
+                    self.raw_images_dir_path,
+                    image
+                    )
 
 
+def generate_secondary_descriptors_hd5(
+    basic_h5_file_path_name,
+    raw_images_dir_path,
+    secondary_h5_file_path_name,
+    thread_num=4
+    ):
 
-if __name__ == "__main__":
-
-
-    basic_file_path = path.analysis_data_dir + 'basic.h5'
-    secondary_file_path = path.analysis_data_dir + 'secondary.h5'
-
-    if os.path.exists(secondary_file_path):
-        os.remove(secondary_file_path)
+    assert os.path.isfile(basic_h5_file_path_name)
+    assert os.path.isdir(raw_images_dir_path)
+    assert not os.path.isfile(secondary_h5_file_path_name)
 
     ## Start Analysis
 
-    with h5py.File(basic_file_path, "a") as input_file_handler, h5py.File(secondary_file_path,
-                                                                          "a") as output_file_handler:
+    with h5py.File(basic_h5_file_path_name, "a") as basic_h5_file_handler, \
+         h5py.File(secondary_h5_file_path_name, "a") as secondary_h5_file_handler:
 
         for molecule_type in (['mrna'], ['protein']):
-            THREAD_NUM = 4
             thread_list = []
-            image_list = helps.preprocess_image_list(input_file_handler, molecule_type)
-            for sub_list in np.array_split(image_list, THREAD_NUM):
-                thread_list.append(Preprocess(sub_list))
+            image_list = helps.preprocess_image_list(
+                basic_h5_file_handler,
+                molecule_type
+                )
+            for sub_list in np.array_split(
+                image_list,
+                thread_num
+                ):
+                thread_list.append(
+                    Preprocess(
+                        basic_h5_file_handler,
+                        secondary_h5_file_handler,
+                        raw_images_dir_path,
+                        sub_list
+                        )
+                    )
 
             for thread in thread_list:
                 thread.start()
 
             for thread in thread_list:
                 thread.join()
+
+    return True
+
+
+if __name__ == "__main__":
+
+    basic_h5_file_path_name = path.analysis_data_dir + 'basic.h5'
+    raw_images_dir_path = path.raw_data_dir
+
+    secondary_h5_file_path_name = path.analysis_data_dir + 'secondary.h5'
+    if os.path.exists(secondary_h5_file_path_name):
+        os.remove(secondary_h5_file_path_name)
+
+    THREAD_NUM = 4
+
+    generate_secondary_descriptors_hd5(
+        basic_h5_file_path_name=basic_h5_file_path_name,
+        raw_images_dir_path=raw_images_dir_path,
+        secondary_h5_file_path_name=secondary_h5_file_path_name,
+        thread_num=THREAD_NUM
+        )
