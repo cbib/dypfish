b76e0c5faa857d198080fb99eb6115e1fbda23fe
Thierry Arsicaud <t.arsicaud@catie.fr>
2019-07-09 17:34:27 +0200

    debugging

--- src/secondary_descriptors.py
+++ src/secondary_descriptors.py
@@ -109,7 +109,7 @@ def set_zero_level(
     raw_images_dir_path
     ):
     molecule, gene, timepoint, number = image.split("/")
-    tubulin_image_path = raw_images_dir_path+gene+'/'+molecule+'_'+timepoint+"/image_"+number+"/tubulin.tif"
+    tubulin_image_path = raw_images_dir_path+'/'+gene+'/'+molecule+'_'+timepoint+"/image_"+number+"/tubulin.tif"
     if not os.path.exists(tubulin_image_path):
         tubulin_image_path = raw_images_dir_path + gene + '/' + molecule + '_' + timepoint +"/"+ number + "/tubulin.tif"
 
