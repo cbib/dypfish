cbib/dypfish

commit ad6ec3bfdf40a6f68591490df3ae6ecb4c4dd154

same fix as d871ddb92a3c2a22b1a97095335befea50a5cdac but for prune intensities

src/helpers.py
@@ -252,10 +252,10 @@ def preprocess_image_list3(file_handler, molecule_type, gene, timepoints):
     for molecule in molecule_type:
         for timepoint in timepoints:
             node = molecule + '/' + gene + '/' + timepoint
-            #if node in file_handler.keys():
-            for image in file_handler[node]:
-                image_path = molecule + '/' + gene + '/' + timepoint + '/' + image
-                image_path_list.append(image_path)
+            if node in file_handler.keys():
+                for image in file_handler[node]:
+                    image_path = molecule + '/' + gene + '/' + timepoint + '/' + image
+                    image_path_list.append(image_path)
     return image_path_list
 
 

src/path.py
@@ -21,7 +21,7 @@ basic_file_basename = 'basic'
 sec_file_basename = 'secondary'
 path_data = raw_data_dir
 #basic_file_path = analysis_data_dir + 'genuine_basic.h5'
-basic_file_path = analysis_data_dir + 'basic_scipy_0.19.0.h5'
+basic_file_path = analysis_data_dir + 'basic.h5'
 secondary_file_path = analysis_data_dir + 'test_secondary.h5'
 mtoc_file_path = analysis_data_dir + 'mtoc.h5'
 h_star_file_path = analysis_data_dir + 'h_star.h5'

commit 3648f3457ae5683a46728c007c42f42c589459ec

fix regression introduced in ad6ec3bfdf40a6f68591490df3ae6ecb4c4dd154 if node in file_handler.keys() only works properly for first level node

src/helpers.py
@@ -254,7 +254,7 @@ def preprocess_image_list3(file_handler, molecule_type, gene, timepoints):
     for molecule in molecule_type:
         for timepoint in timepoints:
             node = molecule + '/' + gene + '/' + timepoint
-            if node in file_handler.keys():
+            if node in file_handler:
                 for image in file_handler[node]:
                     image_path = molecule + '/' + gene + '/' + timepoint + '/' + image
                     image_path_list.append(image_path)