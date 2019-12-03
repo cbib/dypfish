456cfb8f55ea32007be54904a999adacc3657530
benjamin <benjamin.dartigues@u-bordeaux.fr>
2019-12-03 10:05:44 +0100

    Updating last Macha changes

--- analysis/analysis_MTOC/search_enriched_quad.py
+++ analysis/analysis_MTOC/search_enriched_quad.py
@@ -1,44 +1,53 @@
-#!/usr/bin/python
-# encoding: UTF-8
-# author: Benjamin Dartigues
+
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Credits: Benjamin Dartigues, Emmanuel Bouilhol, Hayssam Soueidan, Macha Nikolski
+
+'''Build a dataframe containing mrna and protein normalised distributions between MTOC quadrant,
+   MTOC leading edge quadrant and non MTOC quadrants'''
 
 import h5py
 import numpy as np
 import pandas as pd
 import tqdm
+import argparse
 
 import src.helpers as helps
 import src.image_descriptors as idsc
 import src.path as path
-from src.utils import enable_logger, check_dir,loadconfig
+from src.utils import enable_logger, check_dir, loadconfig
 
 pd.set_option('display.max_rows', 500)
 
-'''build dataframe containing mrna and protein normalised distributions between MTOC quadrant, 
-   MTOC leading edge quadrant and non MTOC quadrant'''
+parser = argparse.ArgumentParser()
+parser.add_argument("--peripheral", "-p", help='boolean flag: perform peripheral computation or not',
+                    action="store_true", default=False)
+args = parser.parse_args()
+is_periph = args.peripheral
 
 
-def main(is_periph=False):
+def main():
     check_dir(path.analysis_dir + 'analysis_MTOC/dataframe/')
     configData = loadconfig("original")
 
-
-    # Required descriptors: spots, IF, cell mask an height_map
+    # Required descriptors: spots, IF, cell mask and height_map
+    # annotations for the number of the quadrant containing the mtoc comes from the mtoc5.h5
     with h5py.File(path.basic_file_path, "r") as file_handler, \
             h5py.File(path.secondary_file_path, "r") as second_file_handler, \
             h5py.File(path.mtoc_file_path, "r") as mtoc_file_handler:
         compute_mrna_counts_per_quadrant(file_handler, is_periph, mtoc_file_handler, second_file_handler, configData)
         compute_protein_counts_per_quadrant(file_handler, is_periph, mtoc_file_handler, second_file_handler, configData)
 
-# TODO check leading edge computation
-def compute_mrna_counts_per_quadrant(file_handler, is_periph, mtoc_file_handler, second_file_handler,configData):
+
+def compute_mrna_counts_per_quadrant(file_handler, is_periph, mtoc_file_handler, second_file_handler, configData):
+    # mrna part
     logger = enable_logger()
     molecule_type = ['/mrna']
     mrnas = configData["GENES"]
     timepoints = configData["TIMEPOINTS_MRNA"]
-    # mrna part
+
     global_mtoc = []
-    global_nmtoc = []
+    global_non_mtoc = []
     global_mtoc_leading = []
     global_mrna = []
     global_image = []
@@ -54,7 +63,7 @@ def compute_mrna_counts_per_quadrant(file_handler, is_periph, mtoc_file_handler,
                     spot_by_quad = idsc.search_periph_mrna_quadrants(file_handler, second_file_handler, image)
                 else:
                     spot_by_quad = idsc.search_mrna_quadrants(file_handler, second_file_handler, image)
-                mtoc_quad_j = idsc.get_mtoc_quad(mtoc_file_handler, image)
+                num_mtoc_quadrant = idsc.get_mtoc_quad(mtoc_file_handler, image)
                 mtoc_spot = spot_by_quad[:, :, 1] == 1
                 non_mtoc_spot = spot_by_quad[:, :, 1] == 0
                 for i in range(90):
@@ -64,14 +73,18 @@ def compute_mrna_counts_per_quadrant(file_handler, is_periph, mtoc_file_handler,
                     global_timepoint.append(timepoint)
                 global_mtoc.extend(spot_by_quad[mtoc_spot][:, 0].flatten())
                 for i in range(0, 270, 3):
-                    global_nmtoc.append(np.mean(spot_by_quad[non_mtoc_spot][:, 0].flatten()[i:i + 2]))
-                if mtoc_quad_j == 1:
+                    # TODO : has to be extend
+                    global_non_mtoc.append(np.mean(spot_by_quad[non_mtoc_spot][:, 0].flatten()[i:i + 3]))
+                # quadrant number is 1 if it is in the leading edge
+                if num_mtoc_quadrant == 1:
                     global_mtoc_leading.extend(spot_by_quad[mtoc_spot][:, 0].flatten())
                 else:
                     for i in range(90):
                         global_mtoc_leading.append(np.nan)
+
+    # TODO: add non_MTOC 1, 2 and 3
     df = pd.DataFrame(
-        {'Image': global_image, 'Gene': global_mrna, 'timepoint': global_timepoint, 'Non MTOC': global_nmtoc,
+        {'Image': global_image, 'Gene': global_mrna, 'timepoint': global_timepoint, 'Non MTOC': global_non_mtoc,
          'MTOC': global_mtoc, 'MTOC leading edge': global_mtoc_leading}, index=global_index)
     if is_periph:
         df.to_csv(
@@ -82,29 +95,28 @@ def compute_mrna_counts_per_quadrant(file_handler, is_periph, mtoc_file_handler,
 
 def compute_protein_counts_per_quadrant(file_handler, is_periph, mtoc_file_handler, sec_file_handler, configData):
     # protein part
+    logger = enable_logger()
     molecule_type = ['/protein']
-    proteins = ["beta_actin", "arhgdia", "gapdh", "pard3"]
-
     proteins = configData["PROTEINS"]
     timepoints = configData["TIMEPOINTS_PROTEIN"]
 
     global_protein = []
     global_mtoc = []
-    global_nmtoc = []
+    global_non_mtoc = []
     global_mtoc_leading = []
     global_image = []
     global_index = []
     global_timepoint = []
-    for protein in tqdm.tqdm(proteins, desc="Proteins"):
-        for timepoint in tqdm.tqdm(timepoints, desc="timepoint"):
+    for protein in tqdm.tqdm(proteins, desc="Processing proteins"):
+        for timepoint in tqdm.tqdm(timepoints, desc="Processing timepoints"):
             image_list = helps.preprocess_image_list3(file_handler, molecule_type, protein, [timepoint])
-            for image in tqdm.tqdm(image_list, desc="image"):
+            for image in tqdm.tqdm(image_list, desc="Processing images"):
                 intensity_by_quad = idsc.search_periph_protein_quadrants(
                     file_handler, sec_file_handler, protein, image, path.path_data) if is_periph else \
-                    idsc.search_protein_quadrants(file_handler,sec_file_handler, mtoc_file_handler, protein, image)
+                    idsc.search_protein_quadrants(file_handler, sec_file_handler, mtoc_file_handler, protein, image)
                 mtoc_intensity = intensity_by_quad[:, :, 1] == 1
                 non_mtoc_intensity = intensity_by_quad[:, :, 1] == 0
-                mtoc_quad_j = idsc.get_mtoc_quad(mtoc_file_handler, image)
+                num_mtoc_quadrant = idsc.get_mtoc_quad(mtoc_file_handler, image)
 
                 for i in range(90):
                     # needed for final boxplot
@@ -114,14 +126,17 @@ def compute_protein_counts_per_quadrant(file_handler, is_periph, mtoc_file_handl
                     global_timepoint.append(timepoint)
                 global_mtoc.extend(intensity_by_quad[mtoc_intensity][:, 0].flatten())
                 for i in range(0, 270, 3):
-                    global_nmtoc.append(np.mean(intensity_by_quad[non_mtoc_intensity][:, 0].flatten()[i:i + 2]))
-                if mtoc_quad_j == 1:
+                    # TODO: has to be extend
+                    global_non_mtoc.append(np.mean(intensity_by_quad[non_mtoc_intensity][:, 0].flatten()[i:i + 3]))
+                if num_mtoc_quadrant == 1:
                     global_mtoc_leading.extend(intensity_by_quad[mtoc_intensity][:, 0].flatten())
                 else:
                     for i in range(90):
                         global_mtoc_leading.append(np.nan)
+
+    # TODO: add non_MTOC 1, 2 and 3
     df = pd.DataFrame({'Image': global_image, 'Gene': global_protein, 'timepoint': global_timepoint,
-                       'Non MTOC': global_nmtoc, 'MTOC': global_mtoc, 'MTOC leading edge': global_mtoc_leading},
+                       'Non MTOC': global_non_mtoc, 'MTOC': global_mtoc, 'MTOC leading edge': global_mtoc_leading},
                       index=global_index)
     if is_periph:
         df.to_csv(
@@ -131,5 +146,4 @@ def compute_protein_counts_per_quadrant(file_handler, is_periph, mtoc_file_handl
 
 
 if __name__ == "__main__":
-    #main()
-    main(is_periph=True)
+    main()
