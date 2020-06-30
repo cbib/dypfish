d4b8ba65dd897bf1e43305f39a2bd147880d9add
Edgar <lefevreedg@gmail.com>
2020-06-30 10:23:06 +0200

    patch download_data_test.py

--- download_data_test.sh
+++ download_data_test.sh
@@ -2,7 +2,7 @@
 
 wget -P data/ http://dypfish.org/file/zip/example_hdf5.zip
 
-unzip data/example_hdf5.zip -d data/example_hdf5/
+unzip -j data/example_hdf5.zip -d data/example_hdf5/
 
 
 rm data/*.zip
