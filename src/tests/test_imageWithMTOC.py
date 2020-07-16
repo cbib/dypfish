61d2bf43b4a51e0931d18d1a7c71d41084301834
ebouilhol <emmanuel.bouilhol@gmail.com>
2020-07-16 09:33:56 +0200

    Patch float problem - unit test

--- src/tests/test_imageWithMTOC.py
+++ src/tests/test_imageWithMTOC.py
@@ -62,7 +62,7 @@ class TestImageWithMTOC(TestCase):
         quadrant_mask = self.img.compute_quadrant_mask(degree=45, slices_num=4, nucleus_centroid=nucleus_centroid,
                                                        image_width=11, image_height=11, cell_mask=cell_mask,
                                                        mtoc_position=mtoc_position)
-        self.assertEqual(quadrant_mask.sum(), 182)
+        self.assertEqual(quadrant_mask.sum(), 179)
 
         quadrant_mask = self.img.compute_quadrant_mask(degree=45, slices_num=4)
         self.assertEqual(quadrant_mask.sum(), 133621)
