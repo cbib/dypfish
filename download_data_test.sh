fcb6257e947c393c40c2fc45d966cc4c36b66996
ebouilhol <emmanuel.bouilhol@gmail.com>
2021-03-22 18:21:36 +0100

    Update paths

--- download_data_test.sh
+++ download_data_test.sh
@@ -1,6 +1,6 @@
 
 
-wget -P data/ http://dypfish.org/file/zip/example_hdf5.zip
+wget -P data/ http://dypfish.org/material-kit-master/file/zip/example_hdf5.zip
 
 unzip -j data/example_hdf5.zip -d data/example_hdf5/
 
