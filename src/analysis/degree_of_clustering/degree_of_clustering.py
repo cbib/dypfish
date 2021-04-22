08c04319caf7d71e839e86ce9b0f608c34b04656
benjamin <benjamin.dartigues@u-bordeaux.fr>
2021-04-22 15:50:43 +0200

    Resolve issue 78 - Add  figures options (stat annot and CI table for all bar plots - add violin plot -Slighty changed config.json add labels for genes and proteins

--- src/analysis/degree_of_clustering/degree_of_clustering.py
+++ src/analysis/degree_of_clustering/degree_of_clustering.py
@@ -102,5 +102,5 @@ if __name__ == '__main__':
         conf_full_path = pathlib.Path(global_root_dir, conf[0])
         constants.init_config(analysis_config_js_path=conf_full_path)
         repo = open_repo()
-        # Use annot=True if you want to add stats annotation in plots
+        # Use annot=True if you want to add stats annotation in your figures
         build_plots(repo, conf, annot=False)
