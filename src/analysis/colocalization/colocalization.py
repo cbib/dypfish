corrected the 3d computation of densities for spots

src/analysis/colocalization/colocalization.py
@@ -3,7 +3,6 @@
 # Credits: Benjamin Dartigues, Emmanuel Bouilhol, Hayssam Soueidan, Macha Nikolski
 
 import pathlib
-import pprint as pp
 import pandas as pd
 from loguru import logger
 import time
@@ -17,16 +16,18 @@ from repository import H5RepositoryWithCheckpoint
 from image_set import ImageSet
 # this should be called as soon as possible
 from path import global_root_dir
-import itertools
-import matplotlib.pyplot as plt
 
-
-def compute_protein_relative_density_per_quadrants_and_slices(analysis_repo, quadrants_num=4):
+def compute_relative_density_per_quadrants_and_slices(analysis_repo, molecule_type, quadrants_num=4):
     protein_cs_dict = {}
     stripes = constants.analysis_config['STRIPE_NUM']
+    if molecule_type == 'mrna':
+        timepoints = constants.dataset_config['TIMEPOINTS_MRNA']
+    else:
+        timepoints = constants.dataset_config['TIMEPOINTS_PROTEIN']
+
     for g in constants.analysis_config['PROTEINS']:
         prot_median = []
-        for timepoint in constants.dataset_config['TIMEPOINTS_PROTEIN']:
+        for timepoint in timepoints:
             image_set = ImageSet(analysis_repo, ["protein/{0}/{1}/".format(g, timepoint)])
             arr = image_set.compute_normalized_quadrant_and_slice_densities(quadrants_num=quadrants_num,
                                                                             stripes=stripes)
@@ -35,22 +36,6 @@ def compute_protein_relative_density_per_quadrants_and_slices(analysis_repo, qua
         protein_cs_dict[g] = prot_median
 
     return protein_cs_dict
-
-
-def compute_mrna_relative_density_per_quadrants_and_slices(analysis_repo, quadrants_num=4):
-    _mrna_cs_dict = {}
-    stripes = constants.analysis_config['STRIPE_NUM']
-    for g in constants.analysis_config['MRNA_GENES']:
-        mrna_median = []
-        for timepoint in constants.dataset_config['TIMEPOINTS_MRNA']:
-            image_set = ImageSet(analysis_repo, ["mrna/{0}/{1}/".format(g, timepoint)])
-            arr = image_set.compute_normalized_quadrant_and_slice_densities(quadrants_num=quadrants_num,
-                                                                            stripes=stripes)
-            mrna_tp_df = pd.DataFrame(arr)
-            mrna_median.append(mrna_tp_df.mean(axis=0).values)
-        _mrna_cs_dict[g] = mrna_median
-
-    return _mrna_cs_dict
 
 
 # configurations contain the order in which the degree of clustering is plotted
@@ -73,21 +58,16 @@ if __name__ == '__main__':
         repo = open_repo()
 
         # Use annot=True if you want to add stats annotation in plots
-        mrna_cs_dict = compute_mrna_relative_density_per_quadrants_and_slices(repo, quadrants_num=8)
-        prot_cs_dict = compute_protein_relative_density_per_quadrants_and_slices(repo, quadrants_num=8)
+        mrna_cs_dict = compute_relative_density_per_quadrants_and_slices(repo, 'mrna', quadrants_num=8)
+        prot_cs_dict = compute_relative_density_per_quadrants_and_slices(repo, 'protein', quadrants_num=8)
 
         css = []
         p_vals = []
         for gene in constants.analysis_config['PROTEINS']:
-            mrna_list = mrna_cs_dict[gene]
-            prot_list = prot_cs_dict[gene]
-
-            (cs, p, ranking) = calculate_colocalization_score(mrna_list,
-                                                              prot_list,
+            cs, p, ranking = calculate_colocalization_score(mrna_cs_dict[gene], prot_cs_dict[gene],
                                                               constants.dataset_config['TIMEPOINTS_NUM_MRNA'],
                                                               constants.dataset_config['TIMEPOINTS_NUM_PROTEIN'],
-                                                              permutation_num=conf[2]
-                                                              )
+                                                              permutation_num=conf[2])
             css.append(cs)
             p_vals.append(p)
             print("gene: ", gene, " p-values (random permutation test): ", p)

src/analysis/colocalization/peripheral_colocalization.py
@@ -6,7 +6,6 @@ import pathlib
 import pprint as pp
 import pandas as pd
 from loguru import logger
-
 import constants
 import plot
 from plot import compute_heatmap
@@ -17,75 +16,48 @@ from repository import H5RepositoryWithCheckpoint
 from image_set import ImageSet
 # this should be called as soon as possible
 from path import global_root_dir
-import itertools
-import matplotlib.pyplot as plt
 
-
-def compute_mrna_peripheral_relative_density_per_quadrants_and_slices(analysis_repo, quadrants_num=4):
-    _mrna_cs_dict = {}
+def compute_peripheral_relative_density_per_quadrants_and_slices(analysis_repo, molecule_type, quadrants_num=4):
+    cs_dict = {}
     stripes = constants.analysis_config['STRIPE_NUM']
-    for g in constants.analysis_config['MRNA_GENES']:
-        mrna_median = []
-        for timepoint in constants.dataset_config['TIMEPOINTS_MRNA']:
-            image_set = ImageSet(analysis_repo, ["mrna/{0}/{1}/".format(g, timepoint)])
+    if molecule_type == 'mrna':
+        timepoints = constants.dataset_config['TIMEPOINTS_MRNA']
+    else:
+        timepoints = constants.dataset_config['TIMEPOINTS_PROTEIN']
+    for gene in constants.analysis_config['PROTEINS']:
+        mean_densities = []
+        for timepoint in timepoints:
+            image_set = ImageSet(analysis_repo, [molecule_type + "/{0}/{1}/".format(gene, timepoint)])
             arr = image_set.compute_peripheral_normalized_quadrant_and_slice_densities(quadrants_num=quadrants_num,
                                                                                        stripes=stripes)
             mrna_tp_df = pd.DataFrame(arr)
-            mrna_median.append(mrna_tp_df.mean(axis=0).values)
-        _mrna_cs_dict[g] = mrna_median
-
-    return _mrna_cs_dict
-
-
-def compute_protein_peripheral_relative_density_per_quadrants_and_slices(analysis_repo, quadrants_num=4):
-    protein_cs_dict = {}
-    stripes = constants.analysis_config['STRIPE_NUM']
-    for g in constants.analysis_config['PROTEINS']:
-        prot_median = []
-        for timepoint in constants.dataset_config['TIMEPOINTS_PROTEIN']:
-            image_set = ImageSet(analysis_repo, ["protein/{0}/{1}/".format(g, timepoint)])
-            arr = image_set.compute_peripheral_normalized_quadrant_and_slice_densities(quadrants_num=quadrants_num,
-                                                                                       stripes=stripes)
-            mrna_tp_df = pd.DataFrame(arr)
-            prot_median.append(mrna_tp_df.mean(axis=0).values)
-        protein_cs_dict[g] = prot_median
-
-    return protein_cs_dict
-
-
-# configurations contain the order in which the degree of clustering is plotted
-configurations = [
-    ["src/analysis/colocalization/config_original_periph.json", []]
-]
+            mean_densities.append(mrna_tp_df.mean(axis=0).values)
+        cs_dict[gene] = mean_densities
+    return cs_dict
 
 # Figure 5C Analysis peripheral Colocalization Score (CS) for original data (5 figures)
 if __name__ == '__main__':
     logger.info("Colocalization Score")
-    for conf in configurations:
-        conf_full_path = pathlib.Path(global_root_dir, conf[0])
-        constants.init_config(analysis_config_js_path=conf_full_path)
-        repo = open_repo()
-        mrna_cs_dict = compute_mrna_peripheral_relative_density_per_quadrants_and_slices(repo, quadrants_num=8)
-        prot_cs_dict = compute_protein_peripheral_relative_density_per_quadrants_and_slices(repo, quadrants_num=8)
+    conf_full_path = pathlib.Path(global_root_dir, "src/analysis/colocalization/config_original_periph.json")
+    constants.init_config(analysis_config_js_path=conf_full_path)
+    repo = open_repo()
+    mrna_cs_dict = compute_peripheral_relative_density_per_quadrants_and_slices(repo, 'mrna', quadrants_num=8)
+    prot_cs_dict = compute_peripheral_relative_density_per_quadrants_and_slices(repo, 'protein', quadrants_num=8)
 
-        css = []
-        p_vals = []
-        for gene in constants.analysis_config['PROTEINS']:
-            mrna_list = mrna_cs_dict[gene]
-            prot_list = prot_cs_dict[gene]
-            (cs, p, ranking) = calculate_colocalization_score(mrna_list, prot_list,
-                                                                     constants.dataset_config['TIMEPOINTS_NUM_MRNA'],
-                                                                     constants.dataset_config['TIMEPOINTS_NUM_PROTEIN'])
-            css.append(cs)
-            p_vals.append(p)
-            tgt_image_name = constants.analysis_config['FIGURE_NAME_FORMAT_CS'].format(gene=gene)
-            tgt_fp = pathlib.Path(constants.analysis_config['FIGURE_OUTPUT_PATH'].format(root_dir=global_root_dir),
-                                  tgt_image_name)
-            compute_heatmap(ranking, gene, tgt_fp)
-
-        tgt_image_name = constants.analysis_config['FIGURE_NAME_FORMAT_CS_HISTOGRAM']
+    css, p_vals = [], []
+    for gene in constants.analysis_config['PROTEINS']:
+        cs, p, ranking = calculate_colocalization_score(mrna_cs_dict[gene], prot_cs_dict[gene],
+                                                            constants.dataset_config['TIMEPOINTS_NUM_MRNA'],
+                                                            constants.dataset_config['TIMEPOINTS_NUM_PROTEIN'])
+        css.append(cs)
+        p_vals.append(p)
+        tgt_image_name = constants.analysis_config['FIGURE_NAME_FORMAT_CS'].format(gene=gene)
         tgt_fp = pathlib.Path(constants.analysis_config['FIGURE_OUTPUT_PATH'].format(root_dir=global_root_dir), tgt_image_name)
-        plot.bar_profile(css, tgt_fp, constants.analysis_config['PLOT_COLORS'])
+        compute_heatmap(ranking, gene, tgt_fp)
+
+    tgt_image_name = constants.analysis_config['FIGURE_NAME_FORMAT_CS_HISTOGRAM']
+    tgt_fp = pathlib.Path(constants.analysis_config['FIGURE_OUTPUT_PATH'].format(root_dir=global_root_dir), tgt_image_name)
+    plot.bar_profile(css, tgt_fp, constants.analysis_config['PLOT_COLORS'])
 
 
 

src/image.py
@@ -233,15 +233,13 @@ class ImageWithMTOC(Image):
         max_density = 0.0
         quadrants_max_MTOC_density = np.zeros((quadrants_num, 2), dtype=float)
         mtoc_position = self.get_mtoc_position()
-        cell_mask = self.get_cell_mask()
 
         degree_span = 360 // quadrants_num
         for degree in range(degree_span):
             quadrant_mask = self.compute_quadrant_mask(degree, quadrants_num)
             mtoc_quad_num = quadrant_mask[mtoc_position[1], mtoc_position[0]]
             # assign each spot to the corresponding quadrant excluding those in the nucleus
-            density_per_quadrant = self.compute_density_per_quadrant(mtoc_quad_num, quadrant_mask, cell_mask
-                                                                     , quadrants_num)
+            density_per_quadrant = self.compute_density_per_quadrant(mtoc_quad_num, quadrant_mask, quadrants_num)
             if density_per_quadrant[mtoc_quad_num - 1, 0] > max_density:
                 max_density = density_per_quadrant[mtoc_quad_num - 1, 0]
                 quadrants_max_MTOC_density = density_per_quadrant
@@ -250,11 +248,10 @@ class ImageWithMTOC(Image):
 
     @helpers.checkpoint_decorator(QUADRANT_DENSITIES_PATH_SUFFIX, dtype=np.float)
     def get_quadrants_densities(self, quadrants_num=4):
-        return self.split_in_quadrants(quadrants_num=quadrants_num)
+        return self.compute_quadrant_densities(quadrants_num=quadrants_num)
 
-    def split_in_quadrants(self, quadrants_num=4) -> np.ndarray:
+    def compute_quadrant_densities(self, quadrants_num=4) -> np.ndarray:
         """
-        was : compute_max_density_MTOC_quadrant
         For all possible subdivisions of the cell in quadrants (90 possible)
         computes the normalized density (vs whole cytoplasm) per quadrant
         and keeps the subdivision such that the MTOC containing quadrant is the densiest.
@@ -267,15 +264,13 @@ class ImageWithMTOC(Image):
         max_density = 0.0
         quadrants_max_MTOC_density = np.zeros((quadrants_num, 2), dtype=float)
         mtoc_position = self.get_mtoc_position()
-        cell_mask = self.get_cell_mask()
 
         degree_span = 360 // quadrants_num
         for degree in range(degree_span):
             quadrant_mask = self.compute_quadrant_mask(degree, quadrants_num)
             mtoc_quad_num = quadrant_mask[mtoc_position[1], mtoc_position[0]]
             # assign each spot to the corresponding quadrant excluding those in the nucleus
-            density_per_quadrant = self.compute_density_per_quadrant(mtoc_quad_num, quadrant_mask, cell_mask
-                                                                     , quadrants_num)
+            density_per_quadrant = self.compute_density_per_quadrant(mtoc_quad_num, quadrant_mask, quadrants_num)
             if density_per_quadrant[mtoc_quad_num - 1, 0] > max_density:
                 max_density = density_per_quadrant[mtoc_quad_num - 1, 0]
                 quadrants_max_MTOC_density = density_per_quadrant
@@ -666,7 +661,7 @@ class ImageWithIntensitiesAndMTOC(ImageWithMTOC, ImageWithIntensities):
 
     def compute_density_per_quadrant(self, mtoc_quad, quadrant_mask, cell_mask, quadrants_num=4) -> np.ndarray:
         """
-        compute volumic density per quadrant;
+        Given a quadrant mask and number of MTOC containing quadrant, compute 2D density per quadrant;
         return an array of values of density paired with the MTOC presence flag (0/1)
         """
         IF = self.get_cytoplasmic_intensities()
@@ -691,7 +686,7 @@ class ImageWithSpotsAndMTOC(ImageWithMTOC, ImageWithSpots):
 
     def compute_density_per_quadrant(self, mtoc_quad, quadrant_mask, cell_mask, quadrants_num=4) -> np.ndarray:
         """
-        compute surfacic density per quadrant;
+        Given an quadrant mask and the number of the MTOC containing quadrant, compute surfacic density per quadrant;
         return an array of values of density paired with the MTOC presence flag (0/1)
         """
         surface_coeff = ((1 / constants.dataset_config['SIZE_COEFFICIENT']) ** 2)

src/image3d.py
@@ -488,47 +488,47 @@ class Image3dWithMTOC(Image3d, ImageWithMTOC):
     def get_peripheral_quadrants_densities(self, quadrants_num=4):
         return self.split_in_quadrants(quadrants_num=quadrants_num, peripheral_flag=True)
 
-    @helpers.checkpoint_decorator(QUADRANT_DENSITIES_PATH_SUFFIX, dtype=np.float)
-    def get_quadrants_densities(self, quadrants_num=4):
-        return self.split_in_quadrants(quadrants_num=quadrants_num, peripheral_flag=False)
-
-    def split_in_quadrants(self, quadrants_num=4, peripheral_flag=False) -> np.ndarray:
-        """
-        was : compute_max_density_MTOC_quadrant
-        For all possible subdivisions of the cell in quadrants (90 possible)
-        computes the normalized density (vs whole cytoplasm) per quadrant
-        and keeps the subdivision such that the MTOC containing quadrant is the densiest.
-        The anchor for the computation is the MTOC containing quadrant.
-        Returns an array with the density values per quadrant and associated MTOC flags
-        """
-        if not quadrants_num in [2, 3, 4, 5, 6, 8, 9]:  # just in case
-            raise (RuntimeError, "Unexpected number of slices (quadrants) %i" % quadrants_num)
-
-        max_density = 0.0
-        quadrants_max_MTOC_density = np.zeros((quadrants_num, 2), dtype=float)
-        mtoc_position = self.get_mtoc_position()
-        height_map = self.adjust_height_map(cytoplasm=True)
-        degree_span = 360 // quadrants_num
-        for degree in range(degree_span):
-            quadrant_mask = self.compute_quadrant_mask(degree, quadrants_num)
-            mtoc_quad_num = quadrant_mask[mtoc_position[1], mtoc_position[0]]
-            # assign each spot to the corresponding quadrant excluding those in the nucleus
-            if (not peripheral_flag):
-                density_per_quadrant = self.compute_density_per_quadrant(mtoc_quad_num, quadrant_mask,
-                                                                         height_map, quadrants_num)
-            else:
-                density_per_quadrant = self.compute_peripheral_density_per_quadrant(mtoc_quad_num, quadrant_mask,
-                                                                                    height_map, quadrants_num)
-
-            if density_per_quadrant[mtoc_quad_num - 1, 0] > max_density:
-                max_density = density_per_quadrant[mtoc_quad_num - 1, 0]
-                quadrants_max_MTOC_density = density_per_quadrant
-
-        return quadrants_max_MTOC_density
-
-    @helpers.checkpoint_decorator(QUADRANT_AND_SLICE_DENSITIES_PATH_SUFFIX, dtype=np.float)
-    def get_quadrants_and_slices_densities(self, quadrants_num=4, stripes=3):
-        return self.split_in_quadrants_and_slices(quadrants_num=quadrants_num, stripes=stripes, peripheral_flag=False)
+    # @helpers.checkpoint_decorator(QUADRANT_DENSITIES_PATH_SUFFIX, dtype=np.float)
+    # def get_quadrants_densities(self, quadrants_num=4):
+    #     return self.split_in_quadrants(quadrants_num=quadrants_num, peripheral_flag=False)
+    #
+    # def split_in_quadrants(self, quadrants_num=4, peripheral_flag=False) -> np.ndarray:
+    #     """
+    #     was : compute_max_density_MTOC_quadrant
+    #     For all possible subdivisions of the cell in quadrants (90 possible)
+    #     computes the normalized density (vs whole cytoplasm) per quadrant
+    #     and keeps the subdivision such that the MTOC containing quadrant is the densiest.
+    #     The anchor for the computation is the MTOC containing quadrant.
+    #     Returns an array with the density values per quadrant and associated MTOC flags
+    #     """
+    #     if not quadrants_num in [2, 3, 4, 5, 6, 8, 9]:  # just in case
+    #         raise (RuntimeError, "Unexpected number of slices (quadrants) %i" % quadrants_num)
+    #
+    #     max_density = 0.0
+    #     quadrants_max_MTOC_density = np.zeros((quadrants_num, 2), dtype=float)
+    #     mtoc_position = self.get_mtoc_position()
+    #     height_map = self.adjust_height_map(cytoplasm=True)
+    #     degree_span = 360 // quadrants_num
+    #     for degree in range(degree_span):
+    #         quadrant_mask = self.compute_quadrant_mask(degree, quadrants_num)
+    #         mtoc_quad_num = quadrant_mask[mtoc_position[1], mtoc_position[0]]
+    #         # assign each spot to the corresponding quadrant excluding those in the nucleus
+    #         if (not peripheral_flag):
+    #             density_per_quadrant = self.compute_density_per_quadrant(mtoc_quad_num, quadrant_mask,
+    #                                                                      height_map, quadrants_num)
+    #         else:
+    #             density_per_quadrant = self.compute_peripheral_density_per_quadrant(mtoc_quad_num, quadrant_mask,
+    #                                                                                 height_map, quadrants_num)
+    #
+    #         if density_per_quadrant[mtoc_quad_num - 1, 0] > max_density:
+    #             max_density = density_per_quadrant[mtoc_quad_num - 1, 0]
+    #             quadrants_max_MTOC_density = density_per_quadrant
+    #
+    #     return quadrants_max_MTOC_density
+    #
+    # @helpers.checkpoint_decorator(QUADRANT_AND_SLICE_DENSITIES_PATH_SUFFIX, dtype=np.float)
+    # def get_quadrants_and_slices_densities(self, quadrants_num=4, stripes=3):
+    #     return self.split_in_quadrants_and_slices(quadrants_num=quadrants_num, stripes=stripes, peripheral_flag=False)
 
     @helpers.checkpoint_decorator(PERIPHERAL_QUADRANT_AND_SLICE_DENSITIES_PATH_SUFFIX, dtype=np.float)
     def get_peripheral_quadrants_and_slices_densities(self, quadrants_num=4, stripes=3):
@@ -577,12 +577,13 @@ class Image3dWithSpotsAndMTOC(Image3dWithMTOC, Image3dWithSpots):
     def is_a(repo: Repository, path: str):
         return Image3dWithMTOC.is_a(repo, path) and Image3dWithSpots.is_a(repo, path)
 
-    def compute_density_per_quadrant(self, mtoc_quad, quadrant_mask, height_map, quadrants_num=4) -> np.ndarray:
+    def compute_density_per_quadrant(self, mtoc_quad, quadrant_mask, quadrants_num=4) -> np.ndarray:
         """
         compute volumic density per quadrant;
         return an array of values of density paired with the MTOC presence flag (0/1)
         """
         volume_coeff = ((1 / constants.dataset_config['SIZE_COEFFICIENT']) ** 2) * 0.3
+        height_map = self.adjust_height_map(cytoplasm=True)
         spots = self.get_cytoplasmic_spots()
         density_per_quadrant = np.zeros((quadrants_num, 2))
         for spot in spots:
@@ -712,8 +713,8 @@ class Image3dWithIntensitiesAndMTOC(Image3dWithMTOC, Image3dWithIntensities):
         """
         IF = self.get_cytoplasmic_intensities()
         cell_mask_dist_map = self.get_cell_mask_distance_map()
-        peripheral_binary_mask = (cell_mask_dist_map > 0) & (
-                cell_mask_dist_map <= peripheral_fraction_threshold).astype(int)
+        peripheral_binary_mask = (cell_mask_dist_map > 0) & \
+                                 (cell_mask_dist_map <= peripheral_fraction_threshold).astype(int)
         IF_periph = np.multiply(IF, peripheral_binary_mask)
         height_map_periph = np.multiply(height_map, peripheral_binary_mask)
         density_per_quadrant = np.zeros((quadrants_num, 2))
@@ -722,9 +723,8 @@ class Image3dWithIntensitiesAndMTOC(Image3dWithMTOC, Image3dWithIntensities):
         density_per_quadrant[mtoc_quad - 1, 1] = 1
         for quad_num in range(quadrants_num):
             density_per_quadrant[quad_num, 0] = np.sum(IF_periph[(quadrant_mask == quad_num + 1)])
-            density_per_quadrant[quad_num, 0] /= np.sum(
-                height_map_periph[(quadrant_mask == quad_num + 1)]) * helpers.volume_coeff()
-
+            density_per_quadrant[quad_num, 0] /= np.sum(height_map_periph[(quadrant_mask == quad_num + 1)]) * \
+                                                 helpers.volume_coeff()
         if density_per_quadrant[:, 1].sum() != 1.0:
             raise (RuntimeError, "error in the MTOC quadrant detection for image %s" % self._path)
 

src/tests/test_image3dWithSpotsAndMTOC.py
@@ -34,23 +34,27 @@ class TestImage3dWithSpotsAndMTOC(TestCase):
         quadrant_mask = self.img.compute_quadrant_mask(45, 4)
         mtoc_position = self.img.get_mtoc_position()
         mtoc_quad = quadrant_mask[mtoc_position[1], mtoc_position[0]]
-        height_map = self.img.adjust_height_map()
-        result = self.img.compute_density_per_quadrant(mtoc_quad, quadrant_mask, height_map)
+        result = self.img.compute_density_per_quadrant(mtoc_quad, quadrant_mask)
 
         self.assertEqual(result.shape, (4, 2))
-        self.assertAlmostEqual(result[1, 0], 0.09460885056155734, places=3)
-        self.assertAlmostEqual(result[:, 0].sum(), 0.4123945214579857, places=3)
+        self.assertAlmostEqual(result[1, 0], 0.13705567289739, places=3)
+        self.assertAlmostEqual(result[:, 0].sum(), 0.5481894332973, places=3)
         self.assertEqual(result[:, 1].sum(), 1.0)
 
-    def test_split_in_quadrants(self):
-        warnings.warn(
-            "test_compute_max_density_MTOC_quadrant test not well tested",
-            RuntimeWarning
-        )
-        test_array = np.array([(0.12947032, 0.), (0.15919367, 0.), (0.06432269, 0.), (0.17692811, 1.)])
-        results = self.img.split_in_quadrants()
-        self.assertEqual(np.shape(results), np.shape(test_array))
-        self.assertAlmostEqual(np.sum(results[:,0].sum()), np.sum(test_array[:,0].sum()), places=7)
+    def test_compute_quadrant_densities(self):
+        result = self.img.compute_quadrant_densities()
+        self.assertAlmostEqual(result[:, 0].sum(), 0.529914777)
+        self.assertEqual(result[3, 1], 1)
+
+    # def test_split_in_quadrants(self):
+    #     warnings.warn(
+    #         "test_compute_max_density_MTOC_quadrant test not well tested",
+    #         RuntimeWarning
+    #     )
+    #     test_array = np.array([(0.12947032, 0.), (0.15919367, 0.), (0.06432269, 0.), (0.17692811, 1.)])
+    #     results = self.img.split_in_quadrants()
+    #     self.assertEqual(np.shape(results), np.shape(test_array))
+    #     self.assertAlmostEqual(np.sum(results[:,0].sum()), np.sum(test_array[:,0].sum()), places=7)
 
     def test_compute_density_per_quadrant_and_slices(self):
         quadrant_mask = self.img.compute_quadrant_mask(45, 4)