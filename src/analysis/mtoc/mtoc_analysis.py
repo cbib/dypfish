b4ade5b17530f4f938691ae16c10a173a4298595
tigloo_123 <macha.nikolski@u-bordeaux.fr>
2021-04-29 22:27:52 +0200

    checked colocalization, peripheral_colocalization and mtoc_analysis

--- src/analysis/mtoc/mtoc_analysis.py
+++ src/analysis/mtoc/mtoc_analysis.py
@@ -3,65 +3,18 @@
 # Credits: Benjamin Dartigues, Emmanuel Bouilhol, Hayssam Soueidan, Macha Nikolski
 
 import pathlib
-from dataclasses import dataclass
-from typing import List
-
 import pandas as pd
 from loguru import logger
 import numpy as np
 import constants
-import mpi_calculator
 import plot
-from helpers import open_repo, create_dir_if_needed_for_filepath, color_variant
+import helpers
 from image_set import ImageSet
 from path import global_root_dir
 from mpi_calculator import DensityStats
 
 # global config
 OUTLIERS_THRESHOLD = 3
-
-def plot_boxplot_MPI(mrna_density_stats: DensityStats, protein_density_stats: DensityStats,
-                     molecule_list, mrna_timepoint_list, protein_timepoint_list, figname):
-    """
-    The timepoint_list has to have the same order as in the mpi() function
-    """
-    plot_colors = constants.analysis_config['PLOT_COLORS']
-    mrna_density_stats.update_group_key(['Timepoint'])
-    protein_density_stats.update_group_key(['Timepoint'])
-
-    for color_num, gene in enumerate(molecule_list):
-        # Collect mrna and protein density statistics for this gene only
-        dd = {'Molecule_type': [], 'Timepoint': [], 'MPI': [], 'err': []}
-        gene_stats = mrna_density_stats.subset_stats("Gene", gene)
-        prot_stats = protein_density_stats.subset_stats("Gene", gene)
-        mrna_mpis, mrna_errs = gene_stats.mpi()
-        prot_mpis, prot_errs = prot_stats.mpi()
-        dd["MPI"] = mrna_mpis + prot_mpis
-        dd["err"] = mrna_errs + prot_errs
-        dd["Molecule_type"] = ["mrna"] * len(mrna_timepoint_list) + ["protein"] * len(protein_timepoint_list)
-        dd["Timepoint"] = sorted(mrna_timepoint_list) + sorted(protein_timepoint_list)
-        for tp in np.setdiff1d(mrna_timepoint_list, protein_timepoint_list):
-            dd["Timepoint"].append(tp)
-            dd["Molecule_type"].append("protein")
-            dd["MPI"].append(0)
-            dd["err"].append(0)
-        for tp in np.setdiff1d(protein_timepoint_list, mrna_timepoint_list):
-            dd["Timepoint"].append(tp)
-            dd["Molecule_type"].append("mrna")
-            dd["MPI"].append(0)
-            dd["err"].append(0)
-
-        df = pd.DataFrame(dd)
-        df.sort_values("Timepoint", axis=0, ascending=True, inplace=True)
-
-        my_pal = {"mrna": str(plot_colors[color_num]),
-                  "protein": str(color_variant(plot_colors[color_num], +80))}
-
-        tgt_image_name = constants.analysis_config['FIGURE_NAME_FORMAT_DYNAMIC_MPI'].format(gene=gene)
-        tgt_fp = pathlib.Path(constants.analysis_config['FIGURE_OUTPUT_PATH'].format(root_dir=global_root_dir),
-                              tgt_image_name)
-        plot.sns_barplot(df, my_pal, tgt_fp, x="Timepoint", y="MPI", hue="Molecule_type", err="err")
-        logger.info("Generated image at {}", str(tgt_fp).split("analysis/")[1])
 
 
 def compute_density_per_quadrant(analysis_repo, molecule_type, groupby_key, quadrants_num, quadrant_labels,
@@ -113,14 +66,16 @@ if __name__ == '__main__':
     for conf in configurations:
         conf_full_path = pathlib.Path(global_root_dir, conf[0])
         constants.init_config(analysis_config_js_path=conf_full_path)
-        repo = open_repo()
+        repo = helpers.open_repo()
         tp_mrna = constants.dataset_config['TIMEPOINTS_MRNA']
         tp_proteins = constants.dataset_config['TIMEPOINTS_PROTEIN']
+        mrna_genes = constants.analysis_config['MRNA_GENES']
+        proteins = constants.analysis_config['PROTEINS']
         mpi_sample_size = constants.analysis_config['MPI_SUB_SAMPLE_SIZE']
 
         group_key = group_keys.get(conf[0], ['Gene'])
         dfs = []
-        for genes, timepoints, molecule_type, quads, in zip([constants.analysis_config['MRNA_GENES'], constants.analysis_config['PROTEINS']], [tp_mrna, tp_proteins],
+        for genes, timepoints, molecule_type, quads, in zip([mrna_genes, proteins], [tp_mrna, tp_proteins],
                                                             ["mrna", "protein"], [4, num_protein_quadrants.get(conf[0], 4)]):
             quadrant_labels = ["Non MTOC" + str(i) for i in range(quads-1)]
             df = compute_density_per_quadrant(analysis_repo=repo, molecule_type=molecule_type,
@@ -140,11 +95,13 @@ if __name__ == '__main__':
             else:
                 tgt_image_name = constants.analysis_config['FIGURE_NAME_FORMAT_PLOT_RATIO'].format(molecule_type=molecule_type)
                 tgt_fp = pathlib.Path(constants.analysis_config['FIGURE_OUTPUT_PATH'].format(root_dir=global_root_dir), tgt_image_name)
-                plot.compute_categorical_violin_plot_ratio(df, molecule_type, tgt_fp, limit_threshold=OUTLIERS_THRESHOLD, term=conf[2], gene=conf[1], groupby_key=[conf[3]])
+                plot.compute_categorical_violin_plot_ratio(df, molecule_type, tgt_fp, limit_threshold=OUTLIERS_THRESHOLD,
+                                                           term=conf[2], gene=conf[1], groupby_key=[conf[3]])
 
                 tgt_image_name = constants.analysis_config['FIGURE_NAME_FORMAT_MTOC_ENRICHMENT'].format(molecule_type=molecule_type)
                 tgt_fp = pathlib.Path(constants.analysis_config['FIGURE_OUTPUT_PATH'].format(root_dir=global_root_dir), tgt_image_name)
-                plot.compute_categorical_violin_plot_enrichment(df, molecule_type, tgt_fp, limit_threshold=OUTLIERS_THRESHOLD, term=conf[2], groupby_key=conf[3])
+                plot.compute_categorical_violin_plot_enrichment(df, molecule_type, tgt_fp, limit_threshold=OUTLIERS_THRESHOLD,
+                                                                term=conf[2], groupby_key=conf[3])
 
             tgt_image_name = constants.analysis_config['FIGURE_NAME_FORMAT_MPI'].format(molecule_type=molecule_type)
             tgt_fp = pathlib.Path(constants.analysis_config['FIGURE_OUTPUT_PATH'].format(root_dir=global_root_dir),
@@ -152,4 +109,4 @@ if __name__ == '__main__':
             plot.plot_MPI(df, molecule_type, tgt_fp)
 
         if "original" in conf[0]:
-            plot_boxplot_MPI(dfs[0], dfs[1], constants.analysis_config['PROTEINS'], tp_mrna, tp_proteins)
+            plot.plot_boxplot_MPI(dfs[0], dfs[1], constants.analysis_config['PROTEINS'], tp_mrna, tp_proteins)
