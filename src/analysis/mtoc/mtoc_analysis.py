b91d6ce9d7995e448b1f3bbcd0d37d6ccb2943ff
benjamin <benjamin.dartigues@u-bordeaux.fr>
2021-05-31 10:38:20 +0200

    add "src/analysis/mtoc/config_periph_prrc2c.json": [Gene, Timepoint] in group_keys to handle prcc2c peripherall procedures

--- src/analysis/mtoc/mtoc_analysis.py
+++ src/analysis/mtoc/mtoc_analysis.py
@@ -19,7 +19,8 @@ OUTLIERS_THRESHOLD = 3
 
 
 def build_labels(quadrants_num):
-    return [f"Non MTOC{i}" for i in range(0, quadrants_num-1)]
+    return [f"Non MTOC{i}" for i in range(0, quadrants_num - 1)]
+
 
 def compute_density_per_quadrant(analysis_repo, molecule_type, groupby_key, quadrants_num, quadrant_labels,
                                  molecule_list, time_points, mpi_sample_size, peripheral_flag=False):
@@ -33,18 +34,19 @@ def compute_density_per_quadrant(analysis_repo, molecule_type, groupby_key, quad
                 logger.warning("Image set is small for {}", molecule)
             res = image_set.compute_normalised_quadrant_densities(quadrants_num=quadrants_num,
                                                                   peripheral_flag=peripheral_flag)
-            mtoc_quadrants = res[res[:,1] == 1][:, 0]
+            mtoc_quadrants = res[res[:, 1] == 1][:, 0]
             num_images = res.shape[0] // quadrants_num
-            non_mtoc_quadrants = res[res[:,1] == 0][:, 0].reshape(num_images, quadrants_num-1)
+            non_mtoc_quadrants = res[res[:, 1] == 0][:, 0].reshape(num_images, quadrants_num - 1)
             density_statictics["Gene"] = [molecule] * num_images
             density_statictics["Timepoint"] = [timepoint] * num_images
             density_statictics["MTOC"] = mtoc_quadrants
-            for i in range(0, quadrants_num-1):
-                density_statictics["Non MTOC" + str(i)] = non_mtoc_quadrants[:,i]
+            for i in range(0, quadrants_num - 1):
+                density_statictics["Non MTOC" + str(i)] = non_mtoc_quadrants[:, i]
             density_per_quadrant.append(pd.DataFrame(density_statictics))
 
     return DensityStats(df=pd.concat(density_per_quadrant), group_key=groupby_key, mpi_sample_size=mpi_sample_size,
                         quadrant_labels=quadrant_labels, mtoc_quadrant_label='MTOC')
+
 
 configurations = [
     ["src/analysis/mtoc/config_original.json", "Gene", [4, 4]],
@@ -58,7 +60,8 @@ configurations = [
 # TODO use this info when building the DensityStats objects
 
 group_keys = {
-    "src/analysis/mtoc/config_prrc2c.json": ['Gene', 'Timepoint']
+    "src/analysis/mtoc/config_prrc2c.json": ['Gene', 'Timepoint'],
+    "src/analysis/mtoc/config_periph_prrc2c.json": ['Gene', 'Timepoint']
 }
 
 # figure 3B left/right mRNA/protein MTOC cytoplasmic enrichment for original data (violin plot)
@@ -85,10 +88,10 @@ if __name__ == '__main__':
             logger.info("Running MPI analysis for {}", conf[0])
             quadrant_labels = build_labels(quadrants_num=quads)
             density_stats = compute_density_per_quadrant(analysis_repo=repo, molecule_type=molecule_type,
-                                                             quadrants_num=quads, quadrant_labels=quadrant_labels,
-                                                             molecule_list=genes, time_points=timepoints,
-                                                             groupby_key=group_key, mpi_sample_size=mpi_sample_size,
-                                                             peripheral_flag=peripheral_flag)
+                                                         quadrants_num=quads, quadrant_labels=quadrant_labels,
+                                                         molecule_list=genes, time_points=timepoints,
+                                                         groupby_key=group_key, mpi_sample_size=mpi_sample_size,
+                                                         peripheral_flag=peripheral_flag)
             dfs.append(density_stats)
 
             tgt_image_name = constants.analysis_config['FIGURE_NAME_FORMAT_MTOC_ENRICHMENT'].format(molecule_type=molecule_type)
