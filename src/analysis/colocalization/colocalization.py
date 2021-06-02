cfe57578af5bbdb4128ea63a6c22d4a8bc67e537
benjamin <benjamin.dartigues@u-bordeaux.fr>
2021-06-02 08:44:50 +0200

    remove cut=0 in violin plot + remove outliers in violin  profile + add dpi=600 option in plt.savefig()

--- src/analysis/colocalization/colocalization.py
+++ src/analysis/colocalization/colocalization.py
@@ -22,7 +22,8 @@ def compute_relative_densities(analysis_repo, molecule_type, quadrants_num=4, pe
     stripes = constants.analysis_config['STRIPE_NUM']
     if peripheral_flag:
         stripes_flag = False; stripes = 1
-    else: stripes_flag = True
+    else:
+        stripes_flag = True
     if molecule_type == 'mrna':
         timepoints = constants.dataset_config['TIMEPOINTS_MRNA']
     else:
@@ -32,7 +33,7 @@ def compute_relative_densities(analysis_repo, molecule_type, quadrants_num=4, pe
         median_densities, gene_clusters = [], []
         for timepoint in timepoints:
             image_set = ImageSet(analysis_repo, [molecule_type + "/{0}/{1}/".format(gene, timepoint)])
-            arr = image_set.compute_normalised_quadrant_densities(quadrants_num=quadrants_num,
+            arr = image_set.compute_normalised_quadrant_densities(quadrants_num=quadrants_num * stripes,
                                                                   peripheral_flag=peripheral_flag,
                                                                   stripes=stripes, stripes_flag=stripes_flag)
             num_images = arr.shape[0] // (quadrants_num * stripes)
