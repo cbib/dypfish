f56715236eccdd4cbd13514f537a2291acb08726
ebouilhol <emmanuel.bouilhol@gmail.com>
2023-06-12 10:43:39 +0200

    Update download_data_test.sh
    Replace broken links

--- download_data_test.sh
+++ download_data_test.sh
@@ -2,7 +2,7 @@
 # Credits: Benjamin Dartigues, Emmanuel Bouilhol, Hayssam Soueidan, Macha Nikolski
 #
 
-wget -P data/ http://dypfish.org/material-kit-master/file/zip/example_hdf5.zip
+wget -P data/ https://zenodo.org/record/8026601/files/example_hdf5.zip?download=1
 
 unzip -j data/example_hdf5.zip -d data/example_hdf5/
 
